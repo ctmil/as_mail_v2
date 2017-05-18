c5cac8863a9933bf0778acaef1e4e9b9e4497662
morrillo <gustavo.orrillo@gmail.com>
2017-05-18 14:24:16 -0300

    Fixed bug

--- models.py
+++ models.py
@@ -19,12 +19,15 @@ class as_mail_message(models.Model):
 
 	@api.one
 	def _compute_mail_type(self):
-		uid = self.env.context['uid']
-		user = self.env['res.users'].browse(uid)
-		if self.author_id.id == user.partner_id.id:
-			self.mail_type = 'outgoing'
+		if 'uid' in self.env.context:
+			uid = self.env.context['uid']
+			user = self.env['res.users'].browse(uid)
+			if self.author_id.id == user.partner_id.id:
+				self.mail_type = 'outgoing'
+			else:
+				self.mail_type = 'incoming'
 		else:
-			self.mail_type = 'incoming'
+			self.mail_type = 'unknown'
 
 	@api.one
 	def _compute_po_id(self):
@@ -36,6 +39,6 @@ class as_mail_message(models.Model):
 		if self.model == 'purchase.request':
 			self.request_id = self.res_id
 
-	mail_type = fields.Selection(selection=[('incoming','Entrante'),('outgoing','Saliente')],compute=_compute_mail_type)
+	mail_type = fields.Selection(selection=[('incoming','Entrante'),('outgoing','Saliente'),('unknown','Desconocido')],compute=_compute_mail_type)
 	po_id = fields.Many2one('purchase.order','Orden de Compra',compute=_compute_po_id)
 	request_id = fields.Many2one('purchase.request','Requisicion',compute=_compute_request_id)
